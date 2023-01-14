67c1e2c3b100612a166c23d106bbba0f9a6459df
Koy Dibb <dibbk5@gmail.com>
2023-01-14 11:47:06 -0700

    Get and Create Fortunes

--- client/main.js
+++ client/main.js
@@ -1,11 +1,33 @@
-const complimentBtn = document.getElementById("complimentButton")
+const complimentBtn = document.getElementById("complimentButton");
+const fortuneBtn = document.getElementById("fortuneButton");
 
 const getCompliment = () => {
-    axios.get("http://localhost:4000/api/compliment/")
-        .then(res => {
-            const data = res.data;
-            alert(data);
-    });
+  axios.get("http://localhost:4000/api/compliment/").then((res) => {
+    const data = res.data;
+    alert(data);
+  });
 };
 
-complimentBtn.addEventListener('click', getCompliment)
+const getFortune = () => {
+  axios.get("http://localhost:4000/api/fortune/").then((res) => {
+    const data = res.data;
+    alert(data);
+  });
+};
+
+const createFortune = () => {
+  axios.post("http://localhost:4000/api/fortune/").then;
+};
+
+// const submitHandler = (e) => {
+//   e.preventDefault();
+
+//   let quote = document.querySelector("#quote");
+
+//   let bodyObj = {
+//     quote: quote.value,
+//   };
+// };
+
+complimentBtn.addEventListener("click", getCompliment);
+fortuneBtn.addEventListener("click", getFortune);
--- server/controller.js
+++ server/controller.js
@@ -1,13 +1,56 @@
-module.exports = {
+let fortunes = [
+  {
+    id: 1,
+    fortune:
+      "A beautiful, smart, and loving person will be coming into your life.",
+  },
+  {
+    id: 2,
+    fortune: "A dubious friend may be an enemy in camouflage.",
+  },
+  {
+    id: 3,
+    fortune: "A faithful friend is a strong defense.",
+  },
+];
+const fortuneID = 4;
 
-    getCompliment: (req, res) => {
-        const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
-      
-        // choose random compliment
-        let randomIndex = Math.floor(Math.random() * compliments.length);
-        let randomCompliment = compliments[randomIndex];
-      
-        res.status(200).send(randomCompliment);
+module.exports = {
+  getCompliment: (req, res) => {
+    const compliments = [
+      "Gee, you're a smart cookie!",
+      "Cool shirt!",
+      "Your Javascript skills are stellar.",
+    ];
+
+    // choose random compliment
+    let randomIndex = Math.floor(Math.random() * compliments.length);
+    let randomCompliment = compliments[randomIndex];
+
+    res.status(200).send(randomCompliment);
+  },
+
+  getFortune: (req, res) => {
+    let randomIndex = Math.floor(Math.random() * fortunes.length);
+    let randomFortune = fortunes[randomIndex].fortune;
+
+    res.status(200).send(randomFortune);
+  },
+
+  createFortune: (req, res) => {
+    let { fortune } = req.body;
+
+    if (!fortune) {
+      return res.status(400).send("Invalid Input");
     }
 
-}
+    const newFortune = {
+      id: fortuneID,
+      fortune,
+    };
+
+    fortunes.push(newFortune);
+    fortuneID += 1;
+    res.status(200).send(fortunes);
+  },
+};
